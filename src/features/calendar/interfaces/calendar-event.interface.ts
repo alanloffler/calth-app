4b7fadb039de5eed5ec519dad889d1f8ee6f6502
alanloffler <alanmatiasloffler@gmail.com>
2026-02-11 16:16:24 -0300

    feat(calendar): status prop

--- src/features/calendar/interfaces/calendar-event.interface.ts
+++ src/features/calendar/interfaces/calendar-event.interface.ts
@@ -1,5 +1,6 @@
 import type { IProfessionalEvent } from "@calendar/interfaces/professional-event.interface";
 import type { IUserEvent } from "@calendar/interfaces/user-event.interface";
+import type { TEventStatus } from "@calendar/enums/event-status.enum";
 
 export interface ICalendarEvent {
   endDate: Date;
@@ -7,6 +8,7 @@ export interface ICalendarEvent {
   professional: IProfessionalEvent;
   professionalId: string;
   startDate: Date;
+  status: TEventStatus;
   title: string;
   user: IUserEvent;
   userId: string;
