873e7eac2b23bf7c64490b1fe867f25b2258882f
alanloffler <alanmatiasloffler@gmail.com>
2026-02-10 22:11:49 -0300

    refactor(roles): editable role by logged user and permissions

--- src/features/roles/views/ViewRole.tsx
+++ src/features/roles/views/ViewRole.tsx
@@ -21,6 +21,7 @@ import { useParams } from "react-router";
 
 import type { IRole, IRolePermissions } from "@roles/interfaces/role.interface";
 import { ERoles } from "@auth/enums/role.enum";
+import { EUserRole } from "@roles/enums/user-role.enum";
 import { RolesService } from "@roles/services/roles.service";
 import { tryCatch } from "@core/utils/try-catch";
 import { useAuthStore } from "@auth/stores/auth.store";
@@ -247,20 +248,23 @@ export default function ViewRole() {
                   </div>
                 ) : (
                   <>
-                    {role?.value !== ERoles.super ? (
+                    {(admin?.role.value === EUserRole.super ||
+                      (admin?.role.value === EUserRole.admin && role?.value !== EUserRole.admin)) && (
+                      <Protected requiredPermission="roles-update">
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Button className="hover:text-edit" size="icon-sm" variant="outline" asChild>
+                              <Link to={`/roles/edit/${id}`}>
+                                <FilePenLine />
+                              </Link>
+                            </Button>
+                          </TooltipTrigger>
+                          <TooltipContent>Editar</TooltipContent>
+                        </Tooltip>
+                      </Protected>
+                    )}
+                    {role?.value !== EUserRole.super && role?.value !== EUserRole.admin && (
                       <>
-                        <Protected requiredPermission="roles-update">
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <Button className="hover:text-edit" size="icon-sm" variant="outline" asChild>
-                                <Link to={`/roles/edit/${id}`}>
-                                  <FilePenLine />
-                                </Link>
-                              </Button>
-                            </TooltipTrigger>
-                            <TooltipContent>Editar</TooltipContent>
-                          </Tooltip>
-                        </Protected>
                         <Protected requiredPermission="roles-delete">
                           <Tooltip>
                             <TooltipTrigger asChild>
@@ -293,21 +297,6 @@ export default function ViewRole() {
                           </Tooltip>
                         </Protected>
                       </>
-                    ) : (
-                      admin?.role.value === ERoles.super && (
-                        <Protected requiredPermission="roles-update">
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <Button className="hover:text-edit" size="icon-sm" variant="outline" asChild>
-                                <Link to={`/roles/edit/${id}`}>
-                                  <FilePenLine />
-                                </Link>
-                              </Button>
-                            </TooltipTrigger>
-                            <TooltipContent>Editar</TooltipContent>
-                          </Tooltip>
-                        </Protected>
-                      )
                     )}
                   </>
                 )}
