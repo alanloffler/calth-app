b9738a2644fc1f68e3c6ce0462a294f64461e172
alanloffler <alanmatiasloffler@gmail.com>
2026-02-12 15:46:56 -0300

    feat(dashboard): latest events component

---
+++ src/features/dashboard/components/LatestEvents.tsx
@@ -0,0 +1,68 @@
+import { Card } from "@components/ui/card";
+import { EventStatus } from "@calendar/components/ui/EventStatus";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table";
+
+import { es } from "date-fns/locale";
+import { format } from "date-fns";
+import { toast } from "sonner";
+import { useCallback, useEffect, useState } from "react";
+
+import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+import { CalendarService } from "@calendar/services/calendar.service";
+import { useTryCatch } from "@core/hooks/useTryCatch";
+
+export function LatestEvents() {
+  const [events, setEvents] = useState<ICalendarEvent[]>();
+  const { isLoading, tryCatch } = useTryCatch();
+
+  const getLatestEvents = useCallback(async () => {
+    const [response, error] = await tryCatch(CalendarService.findAllByBusiness(5));
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    if (response && response.statusCode === 200) {
+      setEvents(response.data);
+    }
+  }, [tryCatch]);
+
+  useEffect(() => {
+    getLatestEvents();
+  }, [getLatestEvents]);
+
+  if (isLoading) return <>Is Loading</>;
+
+  return (
+    <Card className="gap-4 px-6">
+      <h2 className="font-semibold">Últimos turnos</h2>
+      <Table>
+        <TableHeader className="dark:bg-primary-foreground bg-neutral-100">
+          <TableRow>
+            <TableHead>Fecha</TableHead>
+            <TableHead>Horario</TableHead>
+            <TableHead>Estado</TableHead>
+            <TableHead>Título</TableHead>
+            <TableHead>Profesional</TableHead>
+            <TableHead>Paciente</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {events?.map((event) => (
+            <TableRow className="hover:cursor-pointer hover:bg-neutral-50/80 dark:hover:bg-neutral-900/50">
+              <TableCell>{format(event.startDate, "P", { locale: es })}</TableCell>
+              <TableCell>{format(event.startDate, "HH:mm", { locale: es }) + " hs."}</TableCell>
+              <TableCell>
+                <EventStatus size="small" variant={event.status} />
+              </TableCell>
+              <TableCell>{event.title}</TableCell>
+              <TableCell>{`${event.professional.professionalProfile?.professionalPrefix} ${event.professional.firstName} ${event.professional.lastName}`}</TableCell>
+              <TableCell>{`${event.user.firstName} ${event.user.lastName}`}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+}
