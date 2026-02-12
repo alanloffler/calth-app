930ab3bf722eff04f1fe3fcfff7182619384298f
alanloffler <alanmatiasloffler@gmail.com>
2026-02-12 15:45:47 -0300

    feat(calendar): dark styles

--- src/features/calendar/components/ui/EventStatus.tsx
+++ src/features/calendar/components/ui/EventStatus.tsx
@@ -12,11 +12,11 @@ const statusVariants = cva("inline-flex items-center justify-center font-medium 
       large: "px-4 py-2 text-base rounded-md",
     },
     variant: {
-      absent: "bg-amber-100 text-amber-600",
-      attended: "bg-green-100 text-green-600",
-      cancelled: "bg-red-100 text-red-600",
-      in_progress: "bg-fuchsia-100 text-fuchsia-600",
-      pending: "bg-neutral-100 text-neutral-600",
+      absent: "bg-amber-100 text-amber-600 dark:bg-amber-950/80 dark:text-amber-400",
+      attended: "bg-green-100 text-green-600 dark:bg-green-950/80 dark:text-green-400",
+      cancelled: "bg-red-100 text-red-600 dark:bg-red-950/80 dark:text-red-400",
+      in_progress: "bg-fuchsia-100 text-fuchsia-600 dark:bg-fuchsia-950/80 dark:text-fuchsia-400",
+      pending: "bg-neutral-100 text-neutral-600 dark:bg-neutral-800/80 dark:text-neutral-400",
     },
   },
   defaultVariants: {
