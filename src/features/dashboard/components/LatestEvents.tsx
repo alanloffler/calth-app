6feb9a132ef51d389d507ca31827d75d5b24d1fe
alanloffler <alanmatiasloffler@gmail.com>
2026-02-12 21:53:55 -0300

    feat(dashboard): manage loading state

--- src/features/dashboard/components/LatestEvents.tsx
+++ src/features/dashboard/components/LatestEvents.tsx
@@ -2,6 +2,7 @@ import { Button } from "@components/ui/button";
 import { Card } from "@components/ui/card";
 import { EventStatus } from "@calendar/components/ui/EventStatus";
 import { Link } from "react-router";
+import { Loader } from "@components/Loader";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table";
 
 import { es } from "date-fns/locale";
@@ -47,45 +48,48 @@ export function LatestEvents({ className }: IProps) {
     getLatestEvents();
   }, [getLatestEvents]);
 
-  // TODO: handle loading UI (check on LatestPatients.tsx too)
-  if (isLoading) return <>Is Loading</>;
-
   return (
-    <Card className={cn("gap-4 px-6", className)}>
+    <Card className={cn("relative gap-4 px-6", className)}>
       <h2 className="font-semibold">Últimos turnos</h2>
-      <Table>
-        <TableHeader className="dark:bg-primary-foreground bg-neutral-100">
-          <TableRow>
-            <TableHead>Fecha</TableHead>
-            <TableHead>Horario</TableHead>
-            <TableHead>Estado</TableHead>
-            <TableHead>Título</TableHead>
-            <TableHead>Profesional</TableHead>
-            <TableHead>Paciente</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {events?.map((event) => (
-            <TableRow
-              className="hover:cursor-pointer hover:bg-neutral-50/80 dark:hover:bg-neutral-900/50"
-              key={event.id}
-              onClick={() => handleSelectEvent(event)}
-            >
-              <TableCell>{format(event.startDate, "dd/MM", { locale: es })}</TableCell>
-              <TableCell>{format(event.startDate, "HH:mm", { locale: es }) + " hs."}</TableCell>
-              <TableCell>
-                <EventStatus size="small" variant={event.status} />
-              </TableCell>
-              <TableCell>{event.title}</TableCell>
-              <TableCell>{`${event.professional.professionalProfile?.professionalPrefix} ${event.professional.firstName} ${event.professional.lastName}`}</TableCell>
-              <TableCell>{`${event.user.firstName} ${event.user.lastName}`}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Button className="text-foreground justify-end" variant="link" asChild>
-        <Link to="/calendar">Ver todos</Link>
-      </Button>
+      {isLoading ? (
+        <Loader absolute size={20} text="Cargando turnos" />
+      ) : (
+        <>
+          <Table>
+            <TableHeader className="dark:bg-primary-foreground bg-neutral-100">
+              <TableRow>
+                <TableHead>Fecha</TableHead>
+                <TableHead>Horario</TableHead>
+                <TableHead>Estado</TableHead>
+                <TableHead>Título</TableHead>
+                <TableHead>Profesional</TableHead>
+                <TableHead>Paciente</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {events?.map((event) => (
+                <TableRow
+                  className="hover:cursor-pointer hover:bg-neutral-50/80 dark:hover:bg-neutral-900/50"
+                  key={event.id}
+                  onClick={() => handleSelectEvent(event)}
+                >
+                  <TableCell>{format(event.startDate, "dd/MM", { locale: es })}</TableCell>
+                  <TableCell>{format(event.startDate, "HH:mm", { locale: es }) + " hs."}</TableCell>
+                  <TableCell>
+                    <EventStatus size="small" variant={event.status} />
+                  </TableCell>
+                  <TableCell>{event.title}</TableCell>
+                  <TableCell>{`${event.professional.professionalProfile?.professionalPrefix} ${event.professional.firstName} ${event.professional.lastName}`}</TableCell>
+                  <TableCell>{`${event.user.firstName} ${event.user.lastName}`}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Button className="text-foreground justify-end" variant="link" asChild>
+            <Link to="/calendar">Ver todos</Link>
+          </Button>
+        </>
+      )}
     </Card>
   );
 }
