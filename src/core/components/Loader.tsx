85425f47ec8c0080bbac855555a3d947df2cf2ea
alanloffler <alanmatiasloffler@gmail.com>
2026-02-12 21:53:33 -0300

    feat(components): absolute loader position

--- src/core/components/Loader.tsx
+++ src/core/components/Loader.tsx
@@ -2,6 +2,7 @@ import { cn } from "@lib/utils";
 import { useTheme } from "@core/providers/theme-provider";
 
 interface IProps {
+  absolute?: boolean;
   className?: string;
   color?: string;
   size?: number;
@@ -9,13 +10,19 @@ interface IProps {
   text?: string;
 }
 
-export function Loader({ className, color, size = 16, spinnerSize = 2, text }: IProps) {
+export function Loader({ absolute, className, color, size = 16, spinnerSize = 2, text }: IProps) {
   const { theme } = useTheme();
 
   const borderColor = color || (theme === "dark" ? "white" : "black");
 
   return (
-    <div className={cn("flex items-center gap-3", className)}>
+    <div
+      className={cn(
+        "flex items-center gap-3",
+        className,
+        absolute && "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+      )}
+    >
       <div
         className="animate-spin rounded-full"
         style={{
