54305588c31611c0fa8be64b1d4a1b594aad3074
alanloffler <alanmatiasloffler@gmail.com>
2026-02-28 17:53:19 -0300

    feat(users): admin edit form

--- src/features/users/views/EditUser.tsx
+++ src/features/users/views/EditUser.tsx
@@ -1,3 +1,4 @@
+import { EditAdminForm } from "@users/components/EditAdminForm";
 import { EditPatientForm } from "@users/components/EditPatientForm";
 import { EditProfessionalForm } from "@users/components/EditProfessionalForm";
 
@@ -10,13 +11,11 @@ export default function EditUser() {
   const userRole = state.role;
   const { id } = useParams();
 
-  console.log(userRole);
-
   if (!id) return null;
 
   return (
     <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[80%]">
-      {userRole === EUserRole["admin"] && <div>Mostrar formulario de edición de admin</div>}
+      {userRole === EUserRole["admin"] && <EditAdminForm userId={id} />}
       {userRole === EUserRole["patient"] && <EditPatientForm userId={id} />}
       {userRole === EUserRole["professional"] && <EditProfessionalForm userId={id} />}
     </div>
