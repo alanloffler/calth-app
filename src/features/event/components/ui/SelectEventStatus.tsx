4c100812d1faf36f6910d937b96aef73e4f02163
alanloffler <alanmatiasloffler@gmail.com>
2026-03-08 20:55:56 -0300

    feat(event): select event status

---
+++ src/features/event/components/ui/SelectEventStatus.tsx
@@ -0,0 +1,30 @@
+import { EventStatus } from "@calendar/components/ui/EventStatus";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@components/ui/select";
+
+import type { Dispatch, SetStateAction } from "react";
+
+import type { TEventStatus } from "@calendar/enums/event-status.enum";
+import { DEventStatus } from "@calendar/dictionaries/status.dictionary";
+
+interface IProps {
+  status: string | undefined;
+  setStatus: Dispatch<SetStateAction<string | undefined>>;
+}
+export function SelectEventStatus({ status, setStatus }: IProps) {
+  return (
+    <Select value={status} onValueChange={(status) => setStatus(status as TEventStatus)}>
+      <SelectTrigger id="eventStatus" className="">
+        <SelectValue placeholder="Estado" className="placeholder:text-foreground! opacity-100!" />
+      </SelectTrigger>
+      <SelectContent>
+        {Object.entries(DEventStatus)
+          .sort((a, b) => a[1].localeCompare(b[1]))
+          .map((key, idx) => (
+            <SelectItem key={idx} value={key[0]}>
+              <EventStatus className="bg-transparent!" variant={key[0] as TEventStatus} />
+            </SelectItem>
+          ))}
+      </SelectContent>
+    </Select>
+  );
+}
