66773a6b3ed2095e54902890de8f0b33556d8017
alanloffler <alanmatiasloffler@gmail.com>
2026-03-08 13:33:19 -0300

    feat(main): queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 // import { StrictMode } from 'react'
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@core/providers/theme-provider.tsx";
+import { createRoot } from "react-dom/client";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <ThemeProvider defaultTheme="light">
-    <App />
-  </ThemeProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider defaultTheme="light">
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>,
   // </StrictMode>,
 );
