649ca30e70d45d0a606f50e4f1c9ed7da78c8c76
alanloffler <alanmatiasloffler@gmail.com>
2026-03-09 19:32:58 -0300

    feat(event): filter params on findEventsFiltered

--- src/features/event/services/events.service.ts
+++ src/features/event/services/events.service.ts
@@ -1,5 +1,8 @@
+import { format } from "date-fns";
+
 import type { IApiResponse } from "@core/interfaces/api-response.interface";
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+import type { IEventFilters } from "@event/interfaces/filters.interface";
 import { apiClient } from "@core/client/client";
 
 class EventsModuleService {
@@ -13,8 +16,26 @@ class EventsModuleService {
     return EventsModuleService.instance;
   }
 
-  public async findAllByBusiness(limit: number): Promise<IApiResponse<ICalendarEvent[]>> {
-    const response = await apiClient.get(`/events/business?limit=${limit}`);
+  public async findEventsFiltered(filters: IEventFilters, limit: number): Promise<IApiResponse<ICalendarEvent[]>> {
+    let queryParams = `limit=${limit}`;
+
+    if (filters.date) {
+      const formatted = format(filters.date, "yyyy-MM-dd");
+      queryParams += `&date=${formatted}`;
+    }
+    if (filters.patientId) {
+      queryParams += `&patientId=${filters.patientId}`;
+    }
+    if (filters.professionalId) {
+      queryParams += `&professionalId=${filters.professionalId}`;
+    }
+    if (filters.status) {
+      queryParams += `&status=${filters.status}`;
+    }
+
+    const path = `/events/filtered?${queryParams}`;
+
+    const response = await apiClient.get(path);
     return response.data;
   }
 }
