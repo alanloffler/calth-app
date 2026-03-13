eb9e09a7dcb093cfe0c326f61dbeed72048a907f
alanloffler <alanmatiasloffler@gmail.com>
2026-03-13 19:58:04 -0300

    feat(calendar): view event dialog on event click

--- src/features/calendar/components/CalendarEventsList.tsx
+++ src/features/calendar/components/CalendarEventsList.tsx
@@ -1,10 +1,14 @@
+import { ViewEventDialog } from "@event/components/ViewEventDialog";
+
 import { es } from "date-fns/locale";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
 import { EventsService } from "@event/services/events.service";
 import { cn } from "@lib/utils";
 import { formatShortDate } from "@core/formatters/date.formatter";
+import { useEventStore } from "@calendar/stores/event.store";
 
 interface IProps {
   className?: string;
@@ -12,6 +16,8 @@ interface IProps {
 }
 
 export function CalendarEventsList({ className, professionalId }: IProps) {
+  const [open, setOpen] = useState<boolean>(false);
+
   const { data: events } = useQuery({
     queryKey: ["events", professionalId],
     queryFn: () => EventsService.findEventsFiltered({ professionalId }, 10),
@@ -19,18 +25,29 @@ export function CalendarEventsList({ className, professionalId }: IProps) {
   });
 
   return (
-    <div className={cn("flex flex-col rounded-lg border", className)}>
-      <h2 className="bg-muted rounded-t-lg border-b px-2 py-2 text-sm font-semibold">Lista de turnos</h2>
-      <ul>
-        {events?.data?.result.map((event) => (
-          <EventItem key={event.id} event={event} />
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={cn("flex flex-col rounded-lg border", className)}>
+        <h2 className="bg-muted rounded-t-lg border-b px-2 py-2 text-sm font-semibold">Lista de turnos</h2>
+        <ul>
+          {events?.data?.result.map((event) => (
+            <EventItem key={event.id} event={event} setOpen={setOpen} />
+          ))}
+        </ul>
+      </div>
+      <ViewEventDialog open={open} setOpen={setOpen} />
+    </>
   );
 }
 
-function EventItem({ event }: { event: ICalendarEvent }) {
+function EventItem({
+  event,
+  setOpen,
+}: {
+  event: ICalendarEvent;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const { setSelectedEvent } = useEventStore();
+
   const colors = {
     absent: "bg-amber-600",
     cancelled: "bg-red-600",
@@ -43,7 +60,10 @@ function EventItem({ event }: { event: ICalendarEvent }) {
     <li>
       <button
         className="hover:bg-muted/50 flex w-full items-center gap-2 border-b px-2 py-1 text-xs"
-        onClick={() => console.log(event.title)}
+        onClick={() => {
+          setSelectedEvent(event);
+          setOpen(true);
+        }}
       >
         <div className={cn("size-1.5 shrink-0 rounded-full", colors[event.status])}></div>
         <span className="min-w-8.75">{formatShortDate(event.startDate, es)}</span>
