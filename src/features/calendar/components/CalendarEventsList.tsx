abc25526d86cf5fc7314a94013301cd19b84f474
alanloffler <alanmatiasloffler@gmail.com>
2026-03-13 19:19:54 -0300

    feat(calendar): status & styles

--- src/features/calendar/components/CalendarEventsList.tsx
+++ src/features/calendar/components/CalendarEventsList.tsx
@@ -15,6 +15,7 @@ export function CalendarEventsList({ className, professionalId }: IProps) {
   const { data: events } = useQuery({
     queryKey: ["events", professionalId],
     queryFn: () => EventsService.findEventsFiltered({ professionalId }, 10),
+    enabled: !!professionalId,
   });
 
   return (
@@ -30,10 +31,24 @@ export function CalendarEventsList({ className, professionalId }: IProps) {
 }
 
 function EventItem({ event }: { event: ICalendarEvent }) {
+  const colors = {
+    absent: "bg-amber-600",
+    cancelled: "bg-red-600",
+    in_progress: "bg-fuchsia-600",
+    pending: "bg-neutral-600",
+    present: "bg-green-600",
+  };
+
   return (
-    <li className="flex gap-2 border-b px-2 py-1 text-xs">
-      <span className="min-w-8.75">{formatShortDate(event.startDate, es)}</span>
-      <span className="truncate">{event.title}</span>
+    <li>
+      <button
+        className="hover:bg-muted/50 flex w-full items-center gap-2 border-b px-2 py-1 text-xs"
+        onClick={() => console.log(event.title)}
+      >
+        <div className={cn("size-1.5 shrink-0 rounded-full", colors[event.status])}></div>
+        <span className="min-w-8.75">{formatShortDate(event.startDate, es)}</span>
+        <span className="truncate">{event.title}</span>
+      </button>
     </li>
   );
 }
