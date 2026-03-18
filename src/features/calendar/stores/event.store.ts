a67ff69e581df4a633b7d00c79be4f26185b42fb
alanloffler <alanmatiasloffler@gmail.com>
2026-03-18 16:13:37 -0300

    feat(calendar): create event sheet open/setOpen

--- src/features/calendar/stores/event.store.ts
+++ src/features/calendar/stores/event.store.ts
@@ -5,6 +5,7 @@ import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interfa
 
 interface EventState {
   events?: ICalendarEvent[];
+  openCreateEventSheet: boolean;
   openEditEventSheet: boolean;
   openViewEventSheet: boolean;
   refreshKey: number;
@@ -13,6 +14,7 @@ interface EventState {
 
 interface EventActions {
   setEvents: (events?: ICalendarEvent[]) => void;
+  setOpenCreateEventSheet: (open: boolean) => void;
   setOpenEditEventSheet: (open: boolean) => void;
   setOpenViewEventSheet: (open: boolean) => void;
   setSelectedEvent: (event: ICalendarEvent | null) => void;
@@ -23,12 +25,14 @@ export const useEventStore = create(
   persist<EventState & EventActions>(
     (set) => ({
       events: undefined,
+      openCreateEventSheet: false,
       openEditEventSheet: false,
       openViewEventSheet: false,
       refreshKey: 0,
       selectedEvent: null,
 
       setEvents: (events) => set({ events }),
+      setOpenCreateEventSheet: (open) => set({ openCreateEventSheet: open }),
       setOpenEditEventSheet: (open) => set({ openEditEventSheet: open }),
       setOpenViewEventSheet: (open) => set({ openViewEventSheet: open }),
       setSelectedEvent: (event) => set({ selectedEvent: event }),
