2676d880a90efadac0c937fff806a61fa9560755
alanloffler <alanmatiasloffler@gmail.com>
2026-03-18 16:13:14 -0300

    refactor(calendar): global create event sheet

--- src/features/calendar/components/Toolbar.tsx
+++ src/features/calendar/components/Toolbar.tsx
@@ -1,12 +1,16 @@
-import { AddEventSheet } from "@calendar/components/sheets/AddEventSheet";
+import { Plus } from "lucide-react";
+
+import { Button } from "@components/ui/button";
 import { DateHeader } from "@calendar/components/DateHeader";
 import { Navigation } from "@calendar/components/Navigation";
+import { Protected } from "@auth/components/Protected";
 
 import type { ToolbarProps } from "react-big-calendar";
 
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
 import type { TView } from "@calendar/interfaces/calendar-view.type";
 import { cn } from "@lib/utils";
+import { useEventStore } from "@calendar/stores/event.store";
 import { useSidebar } from "@components/ui/sidebar";
 
 interface IProps extends ToolbarProps<ICalendarEvent> {
@@ -17,6 +21,7 @@ interface IProps extends ToolbarProps<ICalendarEvent> {
 
 export function Toolbar(props: IProps) {
   const { open } = useSidebar();
+  const { setOpenCreateEventSheet } = useEventStore();
 
   return (
     <div
@@ -28,7 +33,12 @@ export function Toolbar(props: IProps) {
       <DateHeader calendarView={props.calendarView} currentDate={props.currentDate} />
       <div className="flex flex-col items-center gap-3 md:flex-row">
         <Navigation {...props} />
-        <AddEventSheet onCreateEvent={props.onCreateEvent} />
+        <Protected requiredPermission="events-create">
+          <Button className="w-full md:w-auto" onClick={() => setOpenCreateEventSheet(true)}>
+            <Plus className="h-4 w-4" />
+            Turno
+          </Button>
+        </Protected>
       </div>
     </div>
   );
