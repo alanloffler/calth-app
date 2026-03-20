d81480a9525808a600d51b591c77c15dc23a7d34
alanloffler <alanmatiasloffler@gmail.com>
2026-03-20 15:40:47 -0300

    feat(calendar): check recurring availability

--- src/features/calendar/components/ChooseRecurringDate.tsx
+++ src/features/calendar/components/ChooseRecurringDate.tsx
@@ -1,17 +1,27 @@
+import { Activity } from "react";
+import { Button } from "@components/ui/button";
 import { Checkbox } from "@components/ui/checkbox";
 import { FieldLabel } from "@components/ui/field";
-import { Activity, useState } from "react";
+import { Input } from "@components/ui/input";
+
+import { useState } from "react";
 
 export function ChooseRecurringDate() {
   const [display, setDisplay] = useState<boolean>(false);
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="flex items-center gap-3">
-        <Checkbox id="recurring" name="recurring" onChange={() => setDisplay(!display)} />
+        <Checkbox id="recurring" name="recurring" onCheckedChange={() => setDisplay(!display)} />
         <FieldLabel htmlFor="recurring">Recurrente</FieldLabel>
       </div>
       <Activity mode={display ? "visible" : "hidden"}>
-        <div>Here the form inputs</div>
+        <div className="flex items-center gap-3">
+          <Input className="max-w-25" placeholder="Dias" type="number" />
+          <Button type="button" size="sm" variant="secondary">
+            Comprobar disponibilidad
+          </Button>
+        </div>
       </Activity>
     </div>
   );
