586b3180d6b975616e3a718794d13e7db8af1b39
alanloffler <alanmatiasloffler@gmail.com>
2026-03-20 17:19:51 -0300

    feat(calendar): recurring date dialog

--- src/features/calendar/components/ChooseRecurringDate.tsx
+++ src/features/calendar/components/ChooseRecurringDate.tsx
@@ -1,9 +1,19 @@
 import { Activity } from "react";
 import { Button } from "@components/ui/button";
 import { Checkbox } from "@components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@components/ui/dialog";
 import { FieldLabel } from "@components/ui/field";
 import { Input } from "@components/ui/input";
 
+import { es } from "date-fns/locale";
+import { format } from "date-fns";
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
 
@@ -12,11 +22,13 @@ import { tryCatch } from "@core/utils/try-catch";
 
 interface IProps {
   disabled: boolean;
+  selectedDate: string;
 }
 
-export function ChooseRecurringDate({ disabled }: IProps) {
+export function ChooseRecurringDate({ disabled, selectedDate }: IProps) {
   const [days, setDays] = useState<number>(0);
   const [display, setDisplay] = useState<boolean>(false);
+  const [openRecurringDialog, setOpenRecurringDialog] = useState<boolean>(false);
 
   function handleChecked(checked: boolean) {
     setDisplay(checked);
@@ -28,7 +40,7 @@ export function ChooseRecurringDate({ disabled }: IProps) {
       toast.error(error.message);
     }
     if (response && response.statusCode === 200) {
-      toast.success(response.message);
+      setOpenRecurringDialog(true);
     }
   }
 
@@ -37,31 +49,59 @@ export function ChooseRecurringDate({ disabled }: IProps) {
   }, [disabled]);
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex items-center gap-3">
-        <Checkbox
-          checked={display}
-          disabled={disabled}
-          id="recurring"
-          name="recurring"
-          onCheckedChange={() => handleChecked(!display)}
-        />
-        <FieldLabel htmlFor="recurring">Recurrente</FieldLabel>
-      </div>
-      <Activity mode={display && !disabled ? "visible" : "hidden"}>
+    <>
+      <div className="flex flex-col gap-2">
         <div className="flex items-center gap-3">
-          <Input
-            className="max-w-25"
-            onChange={(e) => setDays(Number(e.target.value))}
-            placeholder="Dias"
-            type="number"
-            value={days}
+          <Checkbox
+            checked={display}
+            disabled={disabled}
+            id="recurring"
+            name="recurring"
+            onCheckedChange={() => handleChecked(!display)}
           />
-          <Button disabled={days === 0} onClick={handleCheckAvailability} type="button" size="sm" variant="secondary">
-            Comprobar disponibilidad
-          </Button>
+          <FieldLabel htmlFor="recurring">Recurrente</FieldLabel>
         </div>
-      </Activity>
-    </div>
+        <Activity mode={display && !disabled ? "visible" : "hidden"}>
+          <div className="flex items-center gap-3">
+            <Input
+              className="max-w-25"
+              onChange={(e) => setDays(Number(e.target.value))}
+              placeholder="Dias"
+              type="number"
+              value={days}
+            />
+            <Button disabled={days === 0} onClick={handleCheckAvailability} type="button" size="sm" variant="secondary">
+              Comprobar disponibilidad
+            </Button>
+          </div>
+        </Activity>
+      </div>
+      <Dialog open={openRecurringDialog} onOpenChange={setOpenRecurringDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Detalles del turno recurrente</DialogTitle>
+            <DialogDescription className="sr-only"></DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-2">
+            <p>Van a ser creados {days} turnos recurrentes en el mismo día y horario.</p>
+            <ul className="flex flex-col gap-2">
+              <li className="flex gap-2">
+                <span className="font-semibold">Día:</span>
+                <span>{format(selectedDate, "EEEE", { locale: es })}</span>
+              </li>
+              <li className="flex gap-2">
+                <span className="font-semibold">Horario:</span>
+                <span>{format(selectedDate, "HH:mm", { locale: es })}</span>
+              </li>
+            </ul>
+          </div>
+          <DialogFooter>
+            <Button onClick={() => setOpenRecurringDialog(false)} type="button" size="sm" variant="secondary">
+              Cancelar
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
