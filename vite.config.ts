4766b89b667809d92173787246d6723a089d3c90
alanloffler <alanmatiasloffler@gmail.com>
2026-03-22 20:07:20 -0300

    build: exclude folders

--- vite.config.ts
+++ vite.config.ts
@@ -60,5 +60,6 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/test/setup.ts",
+    exclude: ["**/node_modules/**", "**/login/__tests__/**"],
   },
 });
