ff6bc9d21c61df0640f74e956cae1ea9aca6b759
alanloffler <alanmatiasloffler@gmail.com>
2026-03-25 14:16:39 -0300

    feat(components): Badge icon-xs size

--- src/core/components/Badge.tsx
+++ src/core/components/Badge.tsx
@@ -8,6 +8,7 @@ const badgeVariants = cva("inline-flex items-center justify-center font-medium u
       small: "px-2 py-1 text-[10px] rounded-sm",
       large: "px-4 py-2 text-base rounded-md",
       icon: "px-1 text-xs rounded-xs size-4",
+      "icon-xs": "p-1 text-[9px] rounded-xs size-3",
     },
     variant: {
       cyan: "bg-cyan-100 text-cyan-600",
