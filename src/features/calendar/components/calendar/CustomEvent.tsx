e4a3ab89596c27bf899c3b3ea160c915d66a4f5e
alanloffler <alanmatiasloffler@gmail.com>
2026-03-25 11:52:33 -0300

    feat(calendar): CustomEvent for Calendar

---
+++ src/features/calendar/components/calendar/CustomEvent.tsx
@@ -0,0 +1,24 @@
+import { format } from "date-fns";
+
+import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
+import type { TView } from "@calendar/interfaces/calendar-view.type";
+import { cn } from "@lib/utils";
+
+export function CustomEvent({ event, view }: { event: ICalendarEvent; view: TView }) {
+  return (
+    <div className={cn("flex items-start gap-1", view === "month" ? "flex-row" : "flex-col")}>
+      <div className={cn("flex w-full items-center gap-1", view === "week" ? "justify-between" : "justify-start")}>
+        <div className="text-[11px] font-normal">{format(event.startDate, "HH:mm")}</div>
+        {event.recurrentId && (
+          <span
+            className={cn(
+              "inline-block h-2 w-2 shrink-0 rounded-full bg-fuchsia-500",
+              view !== "month" ? "mr-1!" : "mr-0",
+            )}
+          ></span>
+        )}
+      </div>
+      <h3>{event.title}</h3>
+    </div>
+  );
+}
