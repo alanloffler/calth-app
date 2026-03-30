9fcbc175d7471a53e68c270a65f4a36d920f769a
alanloffler <alanmatiasloffler@gmail.com>
2026-03-30 18:31:34 -0300

    feat(business): stepper

--- src/features/business/views/CreateBusiness.tsx
+++ src/features/business/views/CreateBusiness.tsx
@@ -1,14 +1,26 @@
 import { GalleryVerticalEnd } from "lucide-react";
 
+import { Stepper } from "@components/Stepper";
+
 export default function CreateBusiness() {
   return (
-    <section className="flex h-screen w-full flex-col bg-gray-100 p-8">
+    <section className="bg-background flex h-screen w-full flex-col gap-8 p-8">
       <header className="flex gap-5">
         <div className="bg-primary text-sidebar-primary-foreground flex aspect-square h-12 w-12 items-center justify-center rounded-lg">
           <GalleryVerticalEnd className="size-6 text-white" />
         </div>
         <h1 className="flex items-center text-2xl font-semibold">Calth</h1>
       </header>
+      <p className="text-lg">
+        Completa los siguientes formularios para crear tu negocio y comenzar a gestionar tus pacientes y turnos.
+      </p>
+      <section>
+        <Stepper steps={["Title 1", "Title 2", "Title 3"]}>
+          <div>Content 1</div>
+          <div>Content 2</div>
+          <div>Content 3</div>
+        </Stepper>
+      </section>
     </section>
   );
 }
