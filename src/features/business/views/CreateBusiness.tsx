64ee70fb3ca07296da438a370066c48f90861ddf
alanloffler <alanmatiasloffler@gmail.com>
2026-03-30 19:35:28 -0300

    feat(business): submit react-hook-form & validation

--- src/features/business/views/CreateBusiness.tsx
+++ src/features/business/views/CreateBusiness.tsx
@@ -2,11 +2,19 @@ import { GalleryVerticalEnd } from "lucide-react";
 
 import { Stepper } from "@components/Stepper";
 
-import { useEffect, useState } from "react";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import { useEffect } from "react";
 
 export default function CreateBusiness() {
-  function handleSubmit(): void {
-    console.log("Submit forms");
+  const methods = useForm({
+    defaultValues: {
+      email: "",
+    },
+    mode: "onChange",
+  });
+
+  function handleSubmit(data: any): void {
+    console.log("Submit forms", data);
   }
 
   return (
@@ -21,34 +29,42 @@ export default function CreateBusiness() {
         Completa los siguientes formularios para crear tu negocio y comenzar a gestionar tus pacientes y turnos.
       </p>
       <section>
-        <Stepper steps={["Title 1", "Title 2"]} onFinish={handleSubmit}>
-          <StepOneForm />
-          <StepTwoForm />
-          <StepTwoForm />
-        </Stepper>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(handleSubmit)}>
+            <Stepper steps={["Title 1", "Title 2", "Title 3"]} onFinish={methods.handleSubmit(handleSubmit)}>
+              <StepOneForm />
+              <StepTwoForm />
+              <StepTwoForm />
+            </Stepper>
+          </form>
+        </FormProvider>
       </section>
     </section>
   );
 }
 
 export function StepOneForm({ setIsValid }: { setIsValid?: (valid: boolean) => void }) {
-  const [email, setEmail] = useState("");
+  const {
+    register,
+    formState: { errors, isValid },
+  } = useFormContext();
 
   useEffect(() => {
-    const isValid = email.includes("@") && email.length > 5;
     setIsValid?.(isValid);
-  }, [email, setIsValid]);
+  }, [isValid, setIsValid]);
 
   return (
     <div className="space-y-4">
-      <h2 className="text-xl font-bold">Datos de contacto</h2>
-      <input
-        className="border p-2"
-        type="email"
-        placeholder="Tu email..."
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      <div>
+        <label>First Name</label>
+        <input {...register("firstName", { required: true })} className="block border p-2" />
+        {errors.firstName && <span className="text-red-500">Required</span>}
+      </div>
+
+      <div>
+        <label>Email</label>
+        <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} className="block border p-2" />
+      </div>
     </div>
   );
 }
