f5ad6979ef1f9b89932ef04b6774a2325f693372
alanloffler <alanmatiasloffler@gmail.com>
2026-03-31 12:54:10 -0300

    feat(business): create contact schema

---
+++ src/features/business/schemas/create-contact.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const createContactSchema = z.object({
+  email: z.email({ message: "Debes ingresar un email válido" }),
+  phoneNumber: z
+    .string()
+    .nonempty("El número de teléfono es obligatorio")
+    .length(10, "El número de teléfono debe tener 10 dígitos"),
+  whatsAppNumber: z.string().length(10, "El número de WhatsApp debe tener 10 dígitos").optional(),
+  website: z
+    .string()
+    .min(7, "El sitio web debe tener al menos 7 caracteres")
+    .max(50, "El sitio web debe tener como máximo 50 caracteres")
+    .optional(),
+});
