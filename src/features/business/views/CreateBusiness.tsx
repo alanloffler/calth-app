800a587af93bcd55aefc140400c49a9f8b7b09c3
alanloffler <alanmatiasloffler@gmail.com>
2026-03-31 09:22:44 -0300

    feat(business): stepper business form

--- src/features/business/views/CreateBusiness.tsx
+++ src/features/business/views/CreateBusiness.tsx
@@ -1,19 +1,27 @@
 import { GalleryVerticalEnd } from "lucide-react";
 
+import { BusinessForm } from "@business/components/forms/BusinessForm";
+import { FormProvider } from "react-hook-form";
 import { Stepper } from "@components/Stepper";
 
-import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import type z from "zod";
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { createBusinessSchema } from "@business/schemas/create-business.schema";
 
 export default function CreateBusiness() {
-  const methods = useForm({
+  const createBusinessForm = useForm<z.infer<typeof createBusinessSchema>>({
+    resolver: zodResolver(createBusinessSchema),
     defaultValues: {
-      email: "",
+      companyName: "",
+      taxId: "",
     },
     mode: "onChange",
   });
 
-  function handleSubmit(data: any): void {
+  function handleSubmit(data: z.output<typeof createBusinessSchema>): void {
     console.log("Submit forms", data);
   }
 
@@ -28,11 +36,14 @@ export default function CreateBusiness() {
       <p className="text-lg">
         Completa los siguientes formularios para crear tu negocio y comenzar a gestionar tus pacientes y turnos.
       </p>
-      <section className="mt-8">
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(handleSubmit)}>
-            <Stepper steps={["Title 1", "Title 2", "Title 3"]} onFinish={methods.handleSubmit(handleSubmit)}>
-              <StepOneForm />
+      <section className="mx-12 my-8">
+        <FormProvider {...createBusinessForm}>
+          <form onSubmit={createBusinessForm.handleSubmit(handleSubmit)}>
+            <Stepper
+              steps={["Tu negocio", "Title 2", "Title 3"]}
+              onFinish={createBusinessForm.handleSubmit(handleSubmit)}
+            >
+              <BusinessForm />
               <StepTwoForm />
               <StepTwoForm />
             </Stepper>
@@ -40,32 +51,6 @@ export default function CreateBusiness() {
         </FormProvider>
       </section>
     </section>
-  );
-}
-
-export function StepOneForm({ setIsValid }: { setIsValid?: (valid: boolean) => void }) {
-  const {
-    register,
-    formState: { errors, isValid },
-  } = useFormContext();
-
-  useEffect(() => {
-    setIsValid?.(isValid);
-  }, [isValid, setIsValid]);
-
-  return (
-    <div className="space-y-4">
-      <div>
-        <label>First Name</label>
-        <input {...register("firstName", { required: true })} className="block border p-2" />
-        {errors.firstName && <span className="text-red-500">Required</span>}
-      </div>
-
-      <div>
-        <label>Email</label>
-        <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} className="block border p-2" />
-      </div>
-    </div>
   );
 }
 
