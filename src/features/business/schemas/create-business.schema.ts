1d0b762935cb42aecc96811496f91fa5fb26e3e1
alanloffler <alanmatiasloffler@gmail.com>
2026-03-31 10:34:10 -0300

    feat(business): address props schema

--- src/features/business/schemas/create-business.schema.ts
+++ src/features/business/schemas/create-business.schema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const createBusinessSchema = z.object({
+  // Comercial
   companyName: z
     .string()
     .nonempty("La razón social es obligatoria")
@@ -17,4 +18,30 @@ export const createBusinessSchema = z.object({
     .nonempty("La descripción es obligatoria")
     .min(3, "La descripción debe tener al menos 3 caracteres")
     .max(200, "La descripción debe tener como máximo 200 caracteres"),
+  // Address
+  street: z
+    .string()
+    .nonempty("La calle es obligatoria")
+    .min(3, "La calle debe tener al menos 3 caracteres")
+    .max(50, "La calle debe tener como máximo 50 caracteres"),
+  city: z
+    .string()
+    .nonempty("La ciudad es obligatoria")
+    .min(3, "La ciudad debe tener al menos 3 caracteres")
+    .max(50, "La ciudad debe tener menos de 50 caracteres"),
+  province: z
+    .string()
+    .nonempty("La provincia es obligatoria")
+    .min(3, "La provincia debe tener al menos 3 caracteres")
+    .max(50, "La provincia debe tener como máximo 50 caracteres"),
+  country: z
+    .string()
+    .nonempty("El país es obligatorio")
+    .min(3, "El país debe tener al menos 3 caracteres")
+    .max(50, "El país debe tener como máximo 50 caracteres"),
+  zipCode: z
+    .string()
+    .nonempty("El código postal es obligatorio")
+    .min(4, "El código postal debe tener al 4 dígitos")
+    .max(6, "El código postal debe tener como máximo 6 dígitos"),
 });
