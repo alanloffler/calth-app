a847c72855d4d012cd6adff44b9cc5d102dda813
alanloffler <alanmatiasloffler@gmail.com>
2026-04-01 15:39:25 -0300

    todo(business): create localstorage on init

--- src/features/business/views/CreateBusiness.tsx
+++ src/features/business/views/CreateBusiness.tsx
@@ -42,6 +42,7 @@ export default function CreateBusiness() {
       return;
     }
     if (response) {
+      // TODO: create localstorage with app-settings, default to show light mode, icons and tooltips
       toast.success("Negocio creado exitosamente");
     }
   }
