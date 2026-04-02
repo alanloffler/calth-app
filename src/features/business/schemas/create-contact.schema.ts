115ed0d459e917d864cbca66fff0c754b7a68fa1
alanloffler <alanmatiasloffler@gmail.com>
2026-04-02 20:50:47 -0300

    refactor(business): website nullable

--- src/features/business/schemas/create-contact.schema.ts
+++ src/features/business/schemas/create-contact.schema.ts
@@ -7,10 +7,13 @@ export const createContactSchema = z.object({
     .nonempty("El número de teléfono es obligatorio")
     .length(10, "El número de teléfono debe tener 10 dígitos"),
   whatsAppNumber: z.string().length(10, "El número de WhatsApp debe tener 10 dígitos").or(z.literal("")).optional(),
-  website: z
-    .string()
-    .min(7, "El sitio web debe tener al menos 7 caracteres")
-    .max(50, "El sitio web debe tener como máximo 50 caracteres")
-    .or(z.literal(""))
-    .optional(),
+  website: z.preprocess(
+    (val) => (val === "" ? null : val),
+    z
+      .string()
+      .min(7, "El sitio web debe tener al menos 7 caracteres")
+      .max(50, "El sitio web debe tener como máximo 50 caracteres")
+      .nullable()
+      .optional(),
+  ),
 });
