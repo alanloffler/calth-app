37512d5e1aac92ce7e9aaf6f55dbe8f69e726fa6
alanloffler <alanmatiasloffler@gmail.com>
2026-04-02 21:17:39 -0300

    refactor(business): optional/nullable property website

--- src/features/business/schemas/create-contact.schema.ts
+++ src/features/business/schemas/create-contact.schema.ts
@@ -9,11 +9,6 @@ export const createContactSchema = z.object({
   whatsAppNumber: z.string().length(10, "El número de WhatsApp debe tener 10 dígitos").or(z.literal("")).optional(),
   website: z.preprocess(
     (val) => (val === "" ? null : val),
-    z
-      .string()
-      .min(7, "El sitio web debe tener al menos 7 caracteres")
-      .max(50, "El sitio web debe tener como máximo 50 caracteres")
-      .nullable()
-      .optional(),
+    z.string().min(7, "El sitio web debe tener al menos 7 caracteres").max(50, "El sitio web debe tener como máximo 50 caracteres").nullable().optional(),
   ),
 });
