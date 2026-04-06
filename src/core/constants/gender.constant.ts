2df7fb0c3a9813f2f658a7b3764e066f4a510b70
alanloffler <alanmatiasloffler@gmail.com>
2026-04-06 17:20:01 -0300

    feat(constants): genders es/en

---
+++ src/core/constants/gender.constant.ts
@@ -0,0 +1,10 @@
+export const GENDERS = {
+  es: [
+    { label: "Femenino", value: "female" },
+    { label: "Masculino", value: "male" },
+  ],
+  en: [
+    { label: "Female", value: "female" },
+    { label: "Male", value: "male" },
+  ],
+};
