c80c8fa8bd29380f002dc658dc710fb0860ed22f
alanloffler <alanmatiasloffler@gmail.com>
2026-04-06 16:38:54 -0300

    feat(config): all DataTable configs

--- src/core/config/table.config.ts
+++ src/core/config/table.config.ts
@@ -1,3 +1,19 @@
 export const EventsTableConfig = {
-  limit: 3,
+  limit: 10,
+};
+
+export const HistoryTableConfig = {
+  limit: 5,
+};
+
+export const PermissionsTableConfig = {
+  limit: 10,
+};
+
+export const RolesTableConfig = {
+  limit: 10,
+};
+
+export const UsersTableConfig = {
+  limit: 10,
 };
