c3fc6ace6c9fe57fe419addb721eb11849dc5416
alanloffler <alanmatiasloffler@gmail.com>
2026-04-08 17:40:18 -0300

    refactor(medical-history): comments prop as default <p> tag (notectl)

--- src/features/medical-history/schemas/create-history.schema.ts
+++ src/features/medical-history/schemas/create-history.schema.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 
 export const createHistorySchema = z.object({
   businessId: z.uuid({ message: "El id de la empresa es obligatorio" }),
-  comments: z.string().min(3, "Mínimo 3 caracteres").max(1000, "Máximo 1000 caracteres").nonoptional(),
+  comments: z
+    .string()
+    .refine((val) => val.replace(/<[^>]*>/g, "").trim().length >= 3, "Mínimo 3 caracteres")
+    .refine((val) => val.replace(/<[^>]*>/g, "").trim().length <= 1000, "Máximo 1000 caracteres")
+    .nonoptional(),
   date: z.date({ message: "La fecha es obligatoria" }),
   eventId: z.uuid({ message: "El id del usuario es obligatorio" }).nullish(),
   professionalId: z.uuid({ message: "El profesional es obligatorio" }).nonoptional(),
