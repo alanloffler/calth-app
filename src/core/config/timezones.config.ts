8d29f1a7eff2fa89573dd26244fcba92f271bc0a
alanloffler <alanmatiasloffler@gmail.com>
2026-04-09 19:43:48 -0300

    feat(config): timezones

--- src/core/config/timezones.config.ts
+++ src/core/config/timezones.config.ts
@@ -1,12 +1,14 @@
-export const TimezonesConfig = [
-  // USA
-  { value: "America/New_York", label: "USA - Eastern (New York)" },
-  { value: "America/Chicago", label: "USA - Central (Chicago)" },
-  { value: "America/Denver", label: "USA - Mountain (Denver)" },
-  { value: "America/Phoenix", label: "USA - Mountain (Phoenix, no DST)" },
-  { value: "America/Los_Angeles", label: "USA - Pacific (Los Angeles)" },
-  { value: "America/Anchorage", label: "USA - Alaska (Anchorage)" },
-  { value: "Pacific/Honolulu", label: "USA - Hawaii (Honolulu, no DST)" },
-  // Argentina
-  { value: "America/Argentina/Buenos_Aires", label: "Argentina (Buenos Aires)" },
-];
+export const TimezonesConfig = {
+  timezones: [
+    // USA
+    { value: "America/New_York", label: "USA - Eastern (New York)" },
+    { value: "America/Chicago", label: "USA - Central (Chicago)" },
+    { value: "America/Denver", label: "USA - Mountain (Denver)" },
+    { value: "America/Phoenix", label: "USA - Mountain (Phoenix, no DST)" },
+    { value: "America/Los_Angeles", label: "USA - Pacific (Los Angeles)" },
+    { value: "America/Anchorage", label: "USA - Alaska (Anchorage)" },
+    { value: "Pacific/Honolulu", label: "USA - Hawaii (Honolulu, no DST)" },
+    // Argentina
+    { value: "America/Argentina/Buenos_Aires", label: "Argentina (Buenos Aires)" },
+  ],
+};
