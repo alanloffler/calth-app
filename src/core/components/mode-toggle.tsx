a1bb49a19edb461576e74114cb60fc1192c0c6b2
alanloffler <alanmatiasloffler@gmail.com>
2026-04-09 18:56:17 -0300

    refactor(components): theme dropdown -> theme switcher

--- src/core/components/mode-toggle.tsx
+++ src/core/components/mode-toggle.tsx
@@ -1,7 +1,6 @@
-import { Moon, Sun } from "lucide-react";
+import { Moon, SunMedium } from "lucide-react";
 
-import { Button } from "@components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@components/ui/dropdown-menu";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
 
 import { useAuthStore } from "@auth/stores/auth.store";
 import { useSettingsStore } from "@settings/stores/settings.store";
@@ -10,7 +9,7 @@ import { useTheme } from "@core/providers/theme-provider";
 export function ModeToggle() {
   const admin = useAuthStore((state) => state.admin);
   const { appSettings, updateAppSetting } = useSettingsStore();
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   async function handleThemeChange(theme: "light" | "dark" | "system") {
     setTheme(theme);
@@ -24,19 +23,21 @@ export function ModeToggle() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => handleThemeChange("light")}>Claro</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleThemeChange("dark")}>Oscuro</DropdownMenuItem>
-        {/* <DropdownMenuItem onClick={() => handleThemeChange("system")}>Sistema</DropdownMenuItem> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <SwitchPrimitive.Root
+      onCheckedChange={(checked) => handleThemeChange(checked ? "dark" : "light")}
+      data-slot="switch"
+      className="peer data-[state=checked]:bg-secondary data-[state=unchecked]:bg-secondary focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-secondary inline-flex h-8 w-14 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50"
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className="bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none flex size-7 items-center justify-center rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-3px)] data-[state=unchecked]:translate-x-px"
+      >
+        {theme === "light" ? (
+          <SunMedium className="h-5 w-5 shrink-0" strokeWidth={1.5} />
+        ) : (
+          <Moon className="h-5 w-5 shrink-0" strokeWidth={1.5} />
+        )}
+      </SwitchPrimitive.Thumb>
+    </SwitchPrimitive.Root>
   );
 }
