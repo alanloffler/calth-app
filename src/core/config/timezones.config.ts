1cd20bea10bcab54a15e9e30562217d143b2e653
alanloffler <alanmatiasloffler@gmail.com>
2026-04-09 19:27:43 -0300

    feat(config): timezones

---
+++ src/core/config/timezones.config.ts
@@ -0,0 +1,12 @@
+export const TimezonesConfig = [
+  // USA
+  { value: "America/New_York", label: "USA - Eastern (New York)" },
+  { value: "America/Chicago", label: "USA - Central (Chicago)" },
+  { value: "America/Denver", label: "USA - Mountain (Denver)" },
+  { value: "America/Phoenix", label: "USA - Mountain (Phoenix, no DST)" },
+  { value: "America/Los_Angeles", label: "USA - Pacific (Los Angeles)" },
+  { value: "America/Anchorage", label: "USA - Alaska (Anchorage)" },
+  { value: "Pacific/Honolulu", label: "USA - Hawaii (Honolulu, no DST)" },
+  // Argentina
+  { value: "America/Argentina/Buenos_Aires", label: "Argentina (Buenos Aires)" },
+];
