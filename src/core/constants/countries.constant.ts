99620026171af574002c0ed5f32c9557010bfdb3
alanloffler <alanmatiasloffler@gmail.com>
2026-04-10 17:30:20 -0300

    feat(constants): countries timezones

--- src/core/constants/countries.constant.ts
+++ src/core/constants/countries.constant.ts
@@ -1,4 +1,22 @@
 export const COUNTRIES = [
-  { label: "Argentina", locale: "es", code: "AR" },
-  { label: "United States", locale: "en", code: "US" },
+  {
+    label: "Argentina",
+    locale: "es",
+    code: "AR",
+    timezones: [{ value: "America/Argentina/Buenos_Aires", label: "ARG - Buenos Aires" }],
+  },
+  {
+    label: "United States",
+    locale: "en",
+    code: "US",
+    timezones: [
+      { value: "America/New_York", label: "USA - Eastern (New York)" },
+      { value: "America/Chicago", label: "USA - Central (Chicago)" },
+      { value: "America/Denver", label: "USA - Mountain (Denver)" },
+      { value: "America/Phoenix", label: "USA - Mountain (Phoenix, no DST)" },
+      { value: "America/Los_Angeles", label: "USA - Pacific (Los Angeles)" },
+      { value: "America/Anchorage", label: "USA - Alaska (Anchorage)" },
+      { value: "Pacific/Honolulu", label: "USA - Hawaii (Honolulu, no DST)" },
+    ],
+  },
 ];
