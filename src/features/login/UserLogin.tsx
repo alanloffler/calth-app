e7a06b71c4b677895a0faabdcedc6551ec25ab91
alanloffler <alanmatiasloffler@gmail.com>
2026-04-11 13:22:56 -0300

    feat(login): theme mode toggle

--- src/features/login/UserLogin.tsx
+++ src/features/login/UserLogin.tsx
@@ -1,8 +1,10 @@
 import { LoginForm } from "@login/components/LoginForm";
+import { ModeToggle } from "@components/ModeToggle";
 
 export function UserLogin() {
   return (
-    <div className="flex h-screen w-full items-center justify-center p-4 sm:p-6 md:p-10">
+    <div className="relative flex h-screen w-full items-center justify-center p-4 sm:p-6 md:p-10">
+      <ModeToggle className="absolute top-5 right-5" />
       <LoginForm type="user" />
     </div>
   );
