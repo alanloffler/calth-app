1c4a9d2e2076b059fc56ff645fccfbdc6820d294
alanloffler <alanmatiasloffler@gmail.com>
2026-04-11 20:31:34 -0300

    feat(business): share inputs mock

--- src/features/business/components/ShareModal.tsx
+++ src/features/business/components/ShareModal.tsx
@@ -1,3 +1,5 @@
+import { Send } from "lucide-react";
+
 import { Button } from "@components/ui/button";
 import {
   Dialog,
@@ -7,15 +9,17 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@components/ui/dialog";
+import { Input } from "@components/ui/input";
 
 import type { Dispatch, SetStateAction } from "react";
 
 interface IProps {
+  image: string | null;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export function ShareModal({ open, setOpen }: IProps) {
+export function ShareModal({ image, open, setOpen }: IProps) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="gap-6 sm:min-w-120">
@@ -23,10 +27,36 @@ export function ShareModal({ open, setOpen }: IProps) {
           <DialogTitle>Compartir código QR</DialogTitle>
           <DialogDescription className="sr-only"></DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4">content here</div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-3 rounded-md border p-3 md:flex-row md:items-center md:justify-between">
+            <div className="flex items-center gap-3">
+              {image && <img src={image} alt="QR Code" className="size-10 object-contain" />}
+              <span className="font-medium">E-mail</span>
+            </div>
+            <div className="flex items-center gap-3">
+              <Input className="" placeholder="Ingresar email" />
+              <Button variant="outline">
+                <Send />
+              </Button>
+            </div>
+          </div>
+          {/* TODO: add tooltips */}
+          <div className="flex flex-col gap-3 rounded-md border p-3 md:flex-row md:items-center md:justify-between">
+            <div className="flex items-center gap-3">
+              {image && <img src={image} alt="QR Code" className="size-10 object-contain" />}
+              <span className="font-medium">WhatsApp</span>
+            </div>
+            <div className="flex items-center gap-3">
+              <Input className="" placeholder="Ingresar número" />
+              <Button variant="outline">
+                <Send />
+              </Button>
+            </div>
+          </div>
+        </div>
         <DialogFooter>
-          <Button onClick={() => setOpen(false)} variant="default">
-            Enviar
+          <Button onClick={() => setOpen(false)} variant="secondary">
+            Cerrar
           </Button>
         </DialogFooter>
       </DialogContent>
