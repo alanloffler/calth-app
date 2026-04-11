e23aff55d0039fd2df00f05b41aa4347a933193f
alanloffler <alanmatiasloffler@gmail.com>
2026-04-11 18:56:32 -0300

    feat(components): download qr code image

--- src/core/components/QRCodeGenerator.tsx
+++ src/core/components/QRCodeGenerator.tsx
@@ -23,12 +23,22 @@ export function QRCodeGenerator({ value }: IProps) {
     });
   }, [value]);
 
+  function handleDownload() {
+    if (!canvasRef.current) return;
+
+    const url = canvasRef.current.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "qr-code.png";
+    link.click();
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       <div className="bg-accent flex h-fit w-fit items-center justify-center rounded-lg p-4">
         <canvas ref={canvasRef} />
       </div>
-      <Button size="sm" variant="outline">
+      <Button onClick={handleDownload} size="sm" variant="outline">
         <Download />
         Descargar
       </Button>
