116253f56d14dfa618bb4bb03bd17db5aae08c70
alanloffler <alanmatiasloffler@gmail.com>
2026-04-15 22:33:08 -0300

    refactor(calendar): test structure

--- src/features/calendar/components/calendar/CustomEvent.tsx
+++ src/features/calendar/components/calendar/CustomEvent.tsx
@@ -6,19 +6,20 @@ import { cn } from "@core/lib/utils";
 
 export function CustomEvent({ event, view }: { event: ICalendarEvent; view: TView }) {
   return (
-    <div className={cn("flex items-start gap-1", view === "month" ? "flex-row" : "flex-col")}>
+    // <div className={cn("flex items-start gap-1", view === "month" ? "flex-row" : "flex-col")}>
+    <div className={cn("flex flex-row items-start gap-1")}>
       <div className={cn("flex w-full items-center gap-1", view === "week" ? "justify-between" : "justify-start")}>
-        <div className="text-[11px] font-normal">{format(event.startDate, "HH:mm")}</div>
         {event.recurrentId && (
           <span
             className={cn(
               "bg-recurrent inline-block h-3 w-3 shrink-0 rounded-full border-[1.5px] border-white",
-              view !== "month" ? "mr-1!" : "mr-0",
+              view !== "month" ? "mr-0!" : "mr-0",
             )}
           ></span>
         )}
+        <div className="text-[11px] font-normal">{format(event.startDate, "HH:mm")}</div>
+        <span>{event.title}</span>
       </div>
-      <span>{event.title}</span>
     </div>
   );
 }
