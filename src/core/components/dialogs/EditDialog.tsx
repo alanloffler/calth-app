11a68fa865db30835f028e59aaf31380f8752d71
alanloffler <alanmatiasloffler@gmail.com>
2026-04-17 18:39:39 -0300

    fix(components): remove deprecated props & conditional description render

--- src/core/components/dialogs/EditDialog.tsx
+++ src/core/components/dialogs/EditDialog.tsx
@@ -1,45 +1,26 @@
-import { Button } from "@components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@components/ui/dialog";
-import { Loader } from "@components/Loader";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@components/ui/dialog";
 
 import type { Dispatch, ReactNode, SetStateAction } from "react";
 
 interface IProps {
-  callback: () => void;
   children: ReactNode;
   description?: string;
-  loader?: boolean;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
   title: string;
 }
 
-export function EditDialog({ callback, children, description, loader, open, setOpen, title }: IProps) {
+export function EditDialog({ children, description, open, setOpen, title }: IProps) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="gap-6 sm:min-w-120">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
-          {description && <DialogDescription>{description}</DialogDescription>}
+          <DialogDescription className={description ? "" : "sr-only"}>{description}</DialogDescription>
         </DialogHeader>
         <div className="grid gap-4">
           <div className="flex flex-col gap-1">{children}</div>
         </div>
-        <DialogFooter>
-          <Button onClick={() => setOpen(false)} variant="ghost">
-            Cancelar
-          </Button>
-          <Button disabled={loader} onClick={() => callback()} variant="warning">
-            {loader ? <Loader color="white" text="Guardando" /> : "Guardar"}
-          </Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
