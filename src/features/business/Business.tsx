e1bc1e54086eb73eb3202aa572ab2bd6c435fcd0
alanloffler <alanmatiasloffler@gmail.com>
2026-04-18 18:50:23 -0300

    refactor(business): tryCatch -> useQuery

--- src/features/business/Business.tsx
+++ src/features/business/Business.tsx
@@ -2,45 +2,30 @@ import { BusinessCard } from "@business/components/BusinessCard";
 import { Card } from "@components/ui/card";
 import { ContactCard } from "@business/components/ContactCard";
 import { PageHeader } from "@components/pages/PageHeader";
+import { PageLoader } from "@components/PageLoader";
 import { PatientsCard } from "@business/components/PatientsCard";
 
-import { toast } from "sonner";
-import { useCallback, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-import type { IBusiness } from "@business/interfaces/business.interface";
 import { BusinessService } from "@business/services/business.service";
 import { cn } from "@core/lib/utils";
 import { useAuthStore } from "@auth/stores/auth.store";
 import { useSidebar } from "@components/ui/sidebar";
-import { useTryCatch } from "@core/hooks/useTryCatch";
 
 export default function Business() {
-  const [business, setBusiness] = useState<IBusiness | undefined>(undefined);
-  const { open: sidebarIsOpen } = useSidebar();
-  const { tryCatch: tryCatchBusiness } = useTryCatch();
   const admin = useAuthStore.getState().admin;
+  const { open: sidebarIsOpen } = useSidebar();
 
-  const fetchBusiness = useCallback(async () => {
-    if (!admin) return;
-    const [response, error] = await tryCatchBusiness(BusinessService.findOne(admin?.businessId));
+  const { data: business, isLoading } = useQuery({
+    queryKey: ["business", admin?.businessId],
+    queryFn: () => BusinessService.findOne(admin!.businessId),
+    select: (response) => response.data,
+    enabled: !!admin?.businessId,
+  });
 
-    if (error) {
-      toast.error(error.message);
-      return;
-    }
+  if (isLoading) return <PageLoader className="justify-center" text="Cargando" />;
 
-    if (response && response.statusCode === 200 && response.data) {
-      setBusiness(response.data);
-    }
-  }, [admin, tryCatchBusiness]);
-
-  useEffect(() => {
-    fetchBusiness();
-  }, [fetchBusiness]);
-
-  if (!business) {
-    return null;
-  }
+  if (!business) return null;
 
   return (
     <div className="flex flex-col gap-8">
@@ -57,7 +42,7 @@ export default function Business() {
         <Card className={cn("col-span-1", sidebarIsOpen ? "md:col-span-1 lg:col-span-3" : "md:col-span-3")}>
           <span className="px-6">Card 3</span>
         </Card>
-        {business.users?.length > 0 && (
+        {(business.users?.length ?? 0) > 0 && (
           <PatientsCard
             className={cn("col-span-1", sidebarIsOpen ? "md:col-span-1 lg:col-span-5" : "md:col-span-5")}
             patients={business.users}
