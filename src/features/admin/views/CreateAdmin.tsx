732f9fb46ce4c6201a56c760afb377ac615aecd9
alanloffler <alanmatiasloffler@gmail.com>
2026-04-19 16:26:20 -0300

    delete(admin): deprecated CreateAdmin component

--- src/features/admin/views/CreateAdmin.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateForm } from "@admin/components/CreateForm";
-
-export default function CreateAdmin() {
-  return (
-    <div className="flex w-full flex-col gap-10 lg:w-[80%] xl:w-[60%]">
-      <CreateForm />
-    </div>
-  );
-}
