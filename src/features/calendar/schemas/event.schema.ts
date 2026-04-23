b643d60f783075deb8ff70b4dabc4b3c5996b050
alanloffler <alanmatiasloffler@gmail.com>
2026-04-23 15:29:38 -0300

    fix(calendar): event startDate validation

--- src/features/calendar/schemas/event.schema.ts
+++ src/features/calendar/schemas/event.schema.ts
@@ -3,19 +3,16 @@ import z from "zod";
 export const eventSchema = z
   .object({
     professionalId: z.uuid("El profesional es obligatorio").nonempty("El profesional es obligatorio"),
-    startDate: z
-      .string()
-      .nonempty("La fecha y horario son obligatorios")
-      .refine(
-        (val) => {
-          if (!val) return false;
-          const date = new Date(val);
-          const hours = date.getHours();
-          const minutes = date.getMinutes();
-          return hours !== 0 || minutes !== 0;
-        },
-        { message: "Debe seleccionar un horario" },
-      ),
+    startDate: z.string({ error: "La fecha es obligatoria" }).refine(
+      (val) => {
+        if (!val) return false;
+        const date = new Date(val);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        return hours !== 0 || minutes !== 0;
+      },
+      { message: "El horario es obligatorio" },
+    ),
     title: z.string().nonempty("El título es obligatorio").min(3, "El título debe tener al menos 3 caracteres"),
     userId: z.uuid("El paciente es obligatorio").nonempty("El paciente es obligatorio"),
     recurringDates: z.array(z.string()).optional(),
