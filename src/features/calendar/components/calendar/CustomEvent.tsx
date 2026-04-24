da5fea1bd93501761d621c26efc0f3696e328a45
alanloffler <alanmatiasloffler@gmail.com>
2026-04-24 18:11:22 -0300

    feat(calendar): tooltip

--- src/features/calendar/components/calendar/CustomEvent.tsx
+++ src/features/calendar/components/calendar/CustomEvent.tsx
@@ -1,3 +1,5 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "@components/ui/tooltip";
+
 import { format } from "date-fns";
 
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
@@ -6,21 +8,26 @@ import { cn } from "@core/lib/utils";
 
 export function CustomEvent({ event, view }: { event: ICalendarEvent; view: TView }) {
   return (
-    // <div className={cn("flex items-start gap-1", view === "month" ? "flex-row" : "flex-col")}>
-    <div className={cn("flex flex-row items-start gap-1")}>
-      {/*<div className={cn("flex w-full items-center gap-1", view === "week" ? "justify-between" : "justify-start")}>*/}
-      <div className={cn("flex w-full items-center justify-start gap-1", view === "month" ? "px-1" : "px-2 py-1")}>
-        {event.recurrentId && (
-          <span
-            className={cn(
-              "bg-recurrent inline-block h-3 w-3 shrink-0 rounded-full border-[1.5px] border-white",
-              view !== "month" ? "mr-0!" : "mr-0",
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <div className={cn("flex flex-row items-start gap-1")}>
+          <div className={cn("flex w-full items-center justify-start gap-1", view === "month" ? "px-1" : "px-2 py-1")}>
+            {event.recurrentId && (
+              <span
+                className={cn(
+                  "bg-recurrent inline-block h-3 w-3 shrink-0 rounded-full border-[1.5px] border-white",
+                  view !== "month" ? "mr-0!" : "mr-0",
+                )}
+              ></span>
             )}
-          ></span>
-        )}
-        <div className="text-[11px] font-normal">{format(event.startDate, "HH:mm")}</div>
+            <span className="text-[11px] font-normal">{format(event.startDate, "HH:mm")}</span>
+            <span className="">{event.title}</span>
+          </div>
+        </div>
+      </TooltipTrigger>
+      <TooltipContent align="center">
         <span>{event.title}</span>
-      </div>
-    </div>
+      </TooltipContent>
+    </Tooltip>
   );
 }
