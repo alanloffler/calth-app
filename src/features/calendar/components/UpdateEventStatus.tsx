3ffda0748426e853956c9282602e7ebe8cb88c3d
alanloffler <alanmatiasloffler@gmail.com>
2026-04-24 12:09:28 -0300

    refactor(calendar): useTryCatch -> useMutation update status

--- src/features/calendar/components/UpdateEventStatus.tsx
+++ src/features/calendar/components/UpdateEventStatus.tsx
@@ -2,13 +2,13 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 import type { ICalendarEvent } from "@calendar/interfaces/calendar-event.interface";
 import type { TEventStatus } from "@calendar/enums/event-status.enum";
 import { CalendarService } from "@calendar/services/calendar.service";
 import { DEventStatus } from "@calendar/dictionaries/status.dictionary";
 import { uppercaseFirst } from "@core/formatters/uppercase-first.formatter";
-import { useTryCatch } from "@core/hooks/useTryCatch";
 
 interface IProps {
   event: ICalendarEvent;
@@ -17,31 +17,27 @@ interface IProps {
 
 export function UpdateEventStatus({ event, onEventChange }: IProps) {
   const [status, setStatus] = useState<TEventStatus>(event.status);
-  const { tryCatch: tryCatchUpdateEvent } = useTryCatch();
 
   useEffect(() => {
     setStatus(event.status);
   }, [event]);
 
-  if (!DEventStatus || !event) return;
-
-  async function updateEventStatus(newStatus: TEventStatus): Promise<void> {
-    const previousStatus = status;
-    setStatus(newStatus);
-
-    const [response, error] = await tryCatchUpdateEvent(CalendarService.updateStatus(event.id, newStatus));
-
-    if (error) {
-      setStatus(previousStatus);
-      toast.error(error.message);
-      return;
-    }
-
-    if (response && response.statusCode === 200) {
+  const { mutate: updateEventStatus } = useMutation({
+    mutationKey: ["event-status"],
+    mutationFn: (status: TEventStatus) => CalendarService.updateStatus(event.id, status),
+    onMutate: async (newStatus) => {
+      const previousStatus = status;
+      setStatus(newStatus);
+      return { previousStatus };
+    },
+    onSuccess: (response, newStatus) => {
       onEventChange({ ...event, status: newStatus });
-      toast.success("Estado del turno actualizado");
-    }
-  }
+      toast.success(response.message ?? "Estado del turno actualizado");
+    },
+    onError: (_error, _newStatus, context) => {
+      if (context?.previousStatus) setStatus(context.previousStatus);
+    },
+  });
 
   return (
     <Select value={status} onValueChange={(s) => updateEventStatus(s as TEventStatus)}>
