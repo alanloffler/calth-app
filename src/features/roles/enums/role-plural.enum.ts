6b9f43c31353e00637d60b5e34c1a448a69a009a
alanloffler <alanmatiasloffler@gmail.com>
2026-04-29 20:42:33 -0300

    fix(roles): omit superadmin role

--- src/features/roles/enums/role-plural.enum.ts
+++ src/features/roles/enums/role-plural.enum.ts
@@ -1,6 +1,6 @@
 import type { TUserRole } from "@roles/interfaces/user-role.type";
 
-export const ERolePlural: Record<TUserRole, string> = {
+export const ERolePlural: Partial<Record<TUserRole, string>> = {
   admin: "administradores",
   patient: "pacientes",
   professional: "profesionales",
