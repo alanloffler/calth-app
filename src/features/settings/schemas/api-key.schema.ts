76dfb9f78d06603da91f633cf95d5eeee7107909
alanloffler <alanmatiasloffler@gmail.com>
2026-05-04 20:45:06 -0300

    feat(settings): api key schema

---
+++ src/features/settings/schemas/api-key.schema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const apiKeySchema = z.object({
+  name: z
+    .string()
+    .nonempty("El proveedor no puede estar vacío")
+    .min(3, "El proveedor debe tener al menos 3 caracteres"),
+  key: z.string().nonempty("La clave no puede estar vacía"),
+});
