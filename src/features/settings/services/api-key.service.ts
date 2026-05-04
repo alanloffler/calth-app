fe1bc4a6c1c6e3aa4c0a195aca7cd6a7ba2b9a59
alanloffler <alanmatiasloffler@gmail.com>
2026-05-04 20:45:23 -0300

    feat(settings): create api key service

--- src/features/settings/services/api-key.service.ts
+++ src/features/settings/services/api-key.service.ts
@@ -1,5 +1,8 @@
+import type z from "zod";
+
 import type { IApiKey } from "@settings/interfaces/api-key.interface";
 import type { IApiResponse } from "@core/interfaces/api-response.interface";
+import type { apiKeySchema } from "@settings/schemas/api-key.schema";
 import { apiClient } from "@core/client/client";
 
 class ApiKeyModuleService {
@@ -13,13 +16,18 @@ class ApiKeyModuleService {
     return ApiKeyModuleService.instance;
   }
 
+  public async create(data: z.infer<typeof apiKeySchema>): Promise<IApiResponse<IApiKey[]>> {
+    const response = await apiClient.post("/api-keys", data);
+    return response.data;
+  }
+
   public async findAll(): Promise<IApiResponse<IApiKey[]>> {
     const response = await apiClient.get("/api-keys");
     return response.data;
   }
 
-  public async update(id: string, data: Omit<IApiKey, "id">): Promise<IApiResponse<void>> {
-    const response = await apiClient.patch(`/api-keys/${id}`, { data });
+  public async update(id: string, data: z.infer<typeof apiKeySchema>): Promise<IApiResponse<void>> {
+    const response = await apiClient.patch(`/api-keys/${id}`, data);
     return response.data;
   }
 
