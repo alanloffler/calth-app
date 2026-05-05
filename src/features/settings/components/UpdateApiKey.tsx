6f999862b6b06453457f3fdb4188e5bf4c4b4c71
alanloffler <alanmatiasloffler@gmail.com>
2026-05-05 18:58:23 -0300

    feat(settings): linkedTo & active input form

--- src/features/settings/components/UpdateApiKey.tsx
+++ src/features/settings/components/UpdateApiKey.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@components/ui/button";
+import { Checkbox } from "@components/ui/checkbox";
+import { ClearIconButton } from "@components/ui/ClearIconButton";
 import { Controller } from "react-hook-form";
 import { EditDialog } from "@components/dialogs/EditDialog";
 import { Field, FieldError, FieldGroup, FieldLabel } from "@components/ui/field";
 import { Input } from "@components/ui/input";
 import { Loader } from "@components/Loader";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@components/ui/select";
 
 import type z from "zod";
 import { toast } from "sonner";
@@ -14,6 +17,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import type { IApiKey } from "@settings/interfaces/api-key.interface";
 import { ApiKeyService } from "@settings/services/api-key.service";
+import { LINKED_TO } from "@core/config/api-key.config";
 import { apiKeySchema } from "@settings/schemas/api-key.schema";
 import { queryClient } from "@core/lib/query-client";
 
@@ -24,11 +28,13 @@ interface IProps {
 }
 
 export function UpdateApiKey({ apiKey, open, setOpen }: IProps) {
-  const form = useForm<z.infer<typeof apiKeySchema>>({
+  const form = useForm<z.input<typeof apiKeySchema>>({
     resolver: zodResolver(apiKeySchema),
     defaultValues: {
       name: apiKey.name,
       key: apiKey.key,
+      linkedTo: apiKey.linkedTo ?? "",
+      active: apiKey.active ?? false,
     },
   });
 
@@ -45,22 +51,27 @@ export function UpdateApiKey({ apiKey, open, setOpen }: IProps) {
   });
 
   useEffect(() => {
-    if (!open) {
-      form.reset();
+    if (open) {
+      form.reset({
+        name: apiKey.name,
+        key: apiKey.key,
+        linkedTo: apiKey.linkedTo ?? "",
+        active: apiKey.active ?? false,
+      });
     }
-  }, [open, form]);
+  }, [open, apiKey, form]);
 
   return (
     <EditDialog
       open={open}
       setOpen={setOpen}
-      title="Crear API Key"
-      description="Completá el formulario para crear una nueva API key"
+      title="Editar API Key"
+      description="Modificá el formulario para editar una API key"
       onOpenAutoFocus={(e) => e.preventDefault()}
     >
       <form
         className="flex flex-col gap-6"
-        id="create-apikey"
+        id="update-apikey"
         onSubmit={form.handleSubmit((data) => updateApiKey(data))}
       >
         <FieldGroup>
@@ -86,12 +97,53 @@ export function UpdateApiKey({ apiKey, open, setOpen }: IProps) {
               </Field>
             )}
           />
+          <Controller
+            name="linkedTo"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="linkedTo">Enlazar con</FieldLabel>
+                <div className="flex items-center gap-3">
+                  <Select aria-invalid={fieldState.invalid} value={field.value} onValueChange={field.onChange}>
+                    <SelectTrigger className="w-1/2" id="linkedTo">
+                      <SelectValue placeholder="Seleccionar" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {LINKED_TO.map((lt) => (
+                        <SelectItem key={lt.id} value={lt.value}>
+                          {lt.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <ClearIconButton type="button" state={field.value} onClear={() => field.onChange("")} />
+                </div>
+                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              </Field>
+            )}
+          />
+          <Controller
+            name="active"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="flex flex-row gap-3">
+                <Checkbox
+                  id="active"
+                  className="size-5!"
+                  checked={field.value}
+                  onCheckedChange={(checked) => field.onChange(!!checked)}
+                />
+                <FieldLabel htmlFor="active">Activo</FieldLabel>
+                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              </Field>
+            )}
+          />
         </FieldGroup>
         <div className="flex justify-end gap-4 pt-4">
           <Button type="button" variant="ghost" onClick={() => setOpen(false)}>
             Cancelar
           </Button>
-          <Button disabled={!form.formState.isDirty} form="create-apikey" type="submit" variant="default">
+          <Button disabled={!form.formState.isDirty} form="update-apikey" type="submit" variant="default">
             {isUpdating ? <Loader color="white" text="Guardando" /> : "Guardar"}
           </Button>
         </div>
