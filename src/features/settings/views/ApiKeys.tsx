484262a049562f9fa5dcd52e888a0a54f8c81eec
alanloffler <alanmatiasloffler@gmail.com>
2026-05-05 10:57:18 -0300

    refactor(settings): componetized CreateApiKey

--- src/features/settings/views/ApiKeys.tsx
+++ src/features/settings/views/ApiKeys.tsx
@@ -3,29 +3,22 @@ import { FilePenLine, Plus, Trash2 } from "lucide-react";
 import { Badge } from "@components/Badge";
 import { Button } from "@components/ui/button";
 import { ConfirmDialog } from "@components/dialogs/ConfirmDialog";
-import { Controller } from "react-hook-form";
+import { CreateApiKey } from "@settings/components/CreateApiKey";
 import { DataTable } from "@components/data-table/DataTable";
 import { EditDialog } from "@components/dialogs/EditDialog";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@components/ui/field";
-import { Input } from "@components/ui/input";
-import { Loader } from "@components/Loader";
 import { PageHeader } from "@components/pages/PageHeader";
 import { Protected } from "@auth/components/Protected";
 import { SortableHeader } from "@components/data-table/SortableHeader";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@components/ui/tooltip";
 
-import type z from "zod";
 import type { ColumnDef } from "@tanstack/react-table";
 import { toast } from "sonner";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 import type { IApiKey } from "@settings/interfaces/api-key.interface";
 import { ApiKeyService } from "@settings/services/api-key.service";
 import { DefaultShortTableConfig } from "@core/config/table.config";
-import { apiKeySchema } from "@settings/schemas/api-key.schema";
 import { queryClient } from "@core/lib/query-client";
 
 export default function ApiKeys() {
@@ -33,14 +26,6 @@ export default function ApiKeys() {
   const [openEditDialog, setOpenEditDialog] = useState<boolean>(false);
   const [openRemoveDialog, setOpenRemoveDialog] = useState<boolean>(false);
   const [selectedKey, setSelectedKey] = useState<IApiKey | null>(null);
-
-  const createForm = useForm<z.infer<typeof apiKeySchema>>({
-    resolver: zodResolver(apiKeySchema),
-    defaultValues: {
-      name: "",
-      key: "",
-    },
-  });
 
   const columns: ColumnDef<IApiKey>[] = [
     {
@@ -115,24 +100,6 @@ export default function ApiKeys() {
     select: (response) => response.data,
   });
 
-  useEffect(() => {
-    if (!openCreateDialog) {
-      createForm.reset();
-    }
-  }, [openCreateDialog, createForm]);
-
-  const { mutate: createApiKey, isPending: isCreating } = useMutation({
-    mutationKey: ["api-keys", "create"],
-    mutationFn: (data: z.infer<typeof apiKeySchema>) => ApiKeyService.create(data),
-    onSuccess: (response) => {
-      queryClient.invalidateQueries({ queryKey: ["api-keys"] });
-      toast.success(response.message);
-    },
-    onSettled: () => {
-      setOpenCreateDialog(false);
-    },
-  });
-
   const { mutate: removeApiKey, isPending: isRemoving } = useMutation({
     mutationKey: ["api-keys", "remove"],
     mutationFn: (id: string) => ApiKeyService.remove(id),
@@ -168,51 +135,7 @@ export default function ApiKeys() {
         />
       </div>
       {/* Create dialog */}
-      <EditDialog
-        open={openCreateDialog}
-        setOpen={setOpenCreateDialog}
-        title="Crear API Key"
-        description="Completá el formulario para crear una nueva API key"
-      >
-        <form
-          className="flex flex-col gap-6"
-          id="create-apikey"
-          onSubmit={createForm.handleSubmit((data) => createApiKey(data))}
-        >
-          <FieldGroup>
-            <Controller
-              name="name"
-              control={createForm.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="name">Proveedor</FieldLabel>
-                  <Input aria-invalid={fieldState.invalid} id="name" {...field} />
-                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-                </Field>
-              )}
-            />
-            <Controller
-              name="key"
-              control={createForm.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="key">Clave</FieldLabel>
-                  <Input aria-invalid={fieldState.invalid} id="key" {...field} />
-                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-                </Field>
-              )}
-            />
-          </FieldGroup>
-          <div className="flex justify-end gap-4 pt-4">
-            <Button type="button" variant="ghost" onClick={() => setOpenCreateDialog(false)}>
-              Cancelar
-            </Button>
-            <Button disabled={!createForm.formState.isDirty} form="create-apikey" type="submit" variant="default">
-              {isCreating ? <Loader color="white" text="Guardando" /> : "Guardar"}
-            </Button>
-          </div>
-        </form>
-      </EditDialog>
+      <CreateApiKey open={openCreateDialog} setOpen={setOpenCreateDialog} />
       {selectedKey && (
         <>
           <EditDialog open={openEditDialog} setOpen={setOpenEditDialog} title="Editar API Key" description="">
