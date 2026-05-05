dd22fd7cae22e3c6dabed10b91d6de486e9701e8
alanloffler <alanmatiasloffler@gmail.com>
2026-05-05 18:52:30 -0300

    feat(ui): onClear handle any function

--- src/core/components/ui/ClearIconButton.tsx
+++ src/core/components/ui/ClearIconButton.tsx
@@ -1,22 +1,21 @@
 import { X } from "lucide-react";
 
-import type { Dispatch, SetStateAction } from "react";
 import { cn } from "@core/lib/utils";
 
 interface IProps {
   state: any | undefined;
-  setState: Dispatch<SetStateAction<any | undefined>>;
+  onClear: () => void;
   type?: "button" | "submit" | "reset" | undefined;
 }
 
-export function ClearIconButton({ state, setState, type = "button" }: IProps) {
+export function ClearIconButton({ state, onClear, type = "button" }: IProps) {
   return (
     <button
       className={cn(
         "bg-muted-foreground hover:bg-foreground flex size-4 shrink-0 items-center justify-center rounded-full opacity-0 transition-opacity",
         state && "pointer-events-auto opacity-100",
       )}
-      onClick={() => setState(undefined)}
+      onClick={onClear}
       type={type}
     >
       <X className="text-background size-3" />
