452eed7ac40572cb4ab43240f557cf4eee18e868
alanloffler <alanmatiasloffler@gmail.com>
2026-05-05 18:55:51 -0300

    feat(settings): schema handle empty linkedTo

--- src/features/settings/schemas/api-key.schema.ts
+++ src/features/settings/schemas/api-key.schema.ts
@@ -1,11 +1,8 @@
 import z from "zod";
 
 export const apiKeySchema = z.object({
-  name: z
-    .string()
-    .nonempty("El proveedor no puede estar vacío")
-    .min(3, "El proveedor debe tener al menos 3 caracteres"),
-  key: z.string().nonempty("La clave no puede estar vacía"),
-  linkedTo: z.string().min(2, "El enlace debe tener al menos 2 caracteres").optional(),
+  name: z.string().min(3, "El proveedor debe tener al menos 3 caracteres"),
+  key: z.string().min(1, "La clave no puede estar vacía"),
+  linkedTo: z.union([z.string().min(2, "El enlace debe tener al menos 2 caracteres"), z.literal("")]).optional(),
   active: z.boolean(),
 });
