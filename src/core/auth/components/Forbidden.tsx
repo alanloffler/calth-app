f8e4b4dd0ab61312be12bac5ad95537a874977bc
alanloffler <alanmatiasloffler@gmail.com>
2026-05-20 18:47:09 -0300

    refactor(auth): forbidden to TEUserRole

--- src/core/auth/components/Forbidden.tsx
+++ src/core/auth/components/Forbidden.tsx
@@ -1,20 +1,21 @@
 import type { ReactNode } from "react";
 
-import type { TRole } from "@auth/enums/role.enum";
+import type { EUserRole } from "@roles/enums/user-role.enum";
 import { useAuthStore } from "@auth/stores/auth.store";
 
+type TEUserRole = (typeof EUserRole)[keyof typeof EUserRole];
 type TVariant = "disabled" | "invisible";
 
 interface IProps {
   children: ReactNode;
-  to: TRole[];
+  to: TEUserRole[];
   variant?: TVariant;
 }
 
 export function Forbidden({ children, to, variant = "disabled" }: IProps) {
   const admin = useAuthStore((state) => state.admin);
 
-  if (admin && to.includes(admin.role.value as TRole)) {
+  if (admin && to.includes(admin.role.value as TEUserRole)) {
     if (variant === "disabled") {
       return <div className="pointer-events-none cursor-pointer opacity-50">{children}</div>;
     }
